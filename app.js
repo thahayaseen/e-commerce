57f387448235223a065a519f2ab4870c947f3ea9
thahayaseen <kthahayaseen@gmail.com>
2024-10-12 10:34:16 +0530

    before r

--- app.js
+++ app.js
@@ -15,36 +15,36 @@ app.use(express.json());
 
 // Session setup
 app.use(session({
-    secret: 'thaha',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false } 
-  }));
+  secret: 'thaha',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
 // Middleware to prevent caching
 //for cache handling
 app.use((req, res, next) => {
-    res.set(
-      "Cache-Control",
-      "no-store, no-cache, must-revalidate, proxy-revalidate"
-    );
-    res.set("Pragma", "no-cache");
-    res.set("Expires", "0");
-    res.set("Surrogate-Control", "no-store");
-    next();
-  });
+  res.set(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+  res.set("Pragma", "no-cache");
+  res.set("Expires", "0");
+  res.set("Surrogate-Control", "no-store");
+  next();
+});
 
 
 app.use(passport.initialize());
 app.use(passport.session());
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/ecommewrs')
-    .then(() => {
-        console.log('MongoDB connected successfully');
-    })
-    .catch((err) => {
-        console.error('MongoDB connection error:', err);
-    });
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 
 // Use routes
 
@@ -54,5 +54,5 @@ app.use('/', router);
 // Start the server
 const PORT = 1000; // Default to 1000 if not set
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
